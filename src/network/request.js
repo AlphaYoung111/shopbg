84e3f670fc61742f83832c0bda15c6fcffba889a
aplaaypung29 <729346015@qq.com>
2020-02-07 16:32:15 +0800

    进度条优化

--- src/network/request.js
+++ src/network/request.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 // export function  httpGet(url) {
 //   const baseUrl = 'https://www.liulongbin.top:8888/api/private/v1/'
 //   return new Promise((resolve, reject) => {
-    
+
 //      axios.get(baseUrl+url)
 //        .then(res => {
-      
+
 //       resolve(res)
 //       }).catch(err => {
 //       reject(err)
@@ -29,12 +30,18 @@ import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
     baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
-    timeout:5000
+    timeout: 5000
   })
   instance.interceptors.request.use(config => {
+    NProgress.start()
     //验证token令牌，添加Authorization
     config.headers.Authorization = window.sessionStorage.getItem('token')
-       return config
-     })
+    return config
+  })
+
+  instance.interceptors.response.use(config => {
+    NProgress.done()
+    return config
+  })
   return instance(config)
 }
